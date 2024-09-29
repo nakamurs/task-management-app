671139dedcf25f80f8de33a22dbc72e216cc065d
中村壮太 <sotanakamura@nakamurutanoair.home>
2024-09-29 17:35:55 -0400

    added ErrorViewModel.cs

---
+++ TaskManagerApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TaskManagerApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
